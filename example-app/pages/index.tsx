b52241bd28268702fe278b80cd8b03fe74d4d218
nibbus <nibbus0x@protonmail.com>
2023-04-22 22:08:24 -0400

    Fix linting

--- example-app/pages/index.tsx
+++ example-app/pages/index.tsx
@@ -42,7 +42,7 @@ const Home: NextPage = () => {
 
       return () => clearInterval(intervalId);
     }
-  }, [didPurchaseDog]);
+  }, [didPurchaseDog, tkn]);
 
   useEffect(() => {
     if (didPurchaseCat) {
@@ -56,7 +56,7 @@ const Home: NextPage = () => {
 
       return () => clearInterval(intervalId);
     }
-  }, [didPurchaseCat]);
+  }, [didPurchaseCat, tkn]);
 
   useEffect(() => {
     setTkn(Math.random().toString());
